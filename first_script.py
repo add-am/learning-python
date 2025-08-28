54ec9e7856f026ef0f48fd096d111a5be251a277
add-am <adamshand22@gmail.com>
2025-08-28 15:46:23 +1000

    learning about modules

--- first_script.py
+++ first_script.py
@@ -1,43 +1,8 @@
-class Vehicle:
-    def __init__(self, started = False, speed = 0):
-        self.started = started
-        self.speed = speed
-    
-    def start(self):
-        self.started = True
-        print("Vehicle on")
-    
-    def increase_speed(self, delta):
-        if self.started:
-            self.speed = self.speed + delta
-            print("Vroom")
-        else:
-            print("Turn the vehicle on")
 
+import mymodule as m
 
-class Car(Vehicle):
-    trunk_open = False
+m.my_function()
 
-    def open_trunk(self):
-        self.trunk_open = True
+from mymodule import my_function
 
-    def close_trunk(self):
-        self.trunk_open = False
-
-class Bike(Vehicle):
-    def __init__(self, center_stand_out = False):
-        self.center_stand_out = center_stand_out
-        super().__init__()
-    def start(self):
-        print("Sorry, out of fuel!")
-
-my_bike = Bike()
-
-my_bike.speed
-
-my_bike.increase_speed(10)
-my_bike.start()
-
-my_car = Car()
-
-my_car.trunk_open
+my_function()
